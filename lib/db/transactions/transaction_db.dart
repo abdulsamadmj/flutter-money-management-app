681f99a1c7bf9298c8e605152b0f253eacbecb71
Abdul Samad M J <samadmj4@gmail.com>
2022-01-16 01:00:26 +0530

    Day 24

--- lib/db/transactions/transaction_db.dart
+++ lib/db/transactions/transaction_db.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:money_management_app/models/transaction/transaction_model.dart';
 
@@ -5,6 +6,7 @@ const TRANSACTION_DB_NAME = 'transaction_db';
 
 abstract class TransactionDbFunctions {
   Future<void> addTransaction(TransactionModel obj);
+  Future<List<TransactionModel>> getAllTransactions();
 }
 
 class TransactionDB implements TransactionDbFunctions {
@@ -16,10 +18,27 @@ class TransactionDB implements TransactionDbFunctions {
     return instance;
   }
 
+  ValueNotifier<List<TransactionModel>> transactionListNotifier =
+      ValueNotifier([]);
+
   @override
   Future<void> addTransaction(TransactionModel obj) async {
     final _db = await Hive.openBox<TransactionModel>(TRANSACTION_DB_NAME);
     _db.put(obj.id, obj);
     print(obj.toString());
+  }
+
+  Future<void> refresh() async {
+    final _list = await getAllTransactions();
+    _list.sort((first, second) => second.date.compareTo(first.date));
+    transactionListNotifier.value.clear();
+    transactionListNotifier.value.addAll(_list);
+    transactionListNotifier.notifyListeners();
+  }
+
+  @override
+  Future<List<TransactionModel>> getAllTransactions() async {
+    final _db = await Hive.openBox<TransactionModel>(TRANSACTION_DB_NAME);
+    return _db.values.toList();
   }
 }
--- lib/screens/transactions/screen_transaction.dart
+++ lib/screens/transactions/screen_transaction.dart
@@ -1,27 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:money_management_app/db/category/category_db.dart';
+import 'package:money_management_app/db/transactions/transaction_db.dart';
+import 'package:money_management_app/models/category/category_model.dart';
+import 'package:money_management_app/models/transaction/transaction_model.dart';
 
 class ScreenTransactions extends StatelessWidget {
   const ScreenTransactions({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: const EdgeInsets.all(10),
-      itemBuilder: (ctx, index) {
-        return const Card(
-          child: ListTile(
-            leading: Text('09 Jan'),
-            title: Text('Rs.10000'),
-            subtitle: Text('Category'),
-          ),
+    TransactionDB.instance.refresh();
+    CategoryDb.instance.refreshUI();
+    return ValueListenableBuilder(
+      valueListenable: TransactionDB.instance.transactionListNotifier,
+      builder: (BuildContext ctx, List<TransactionModel> newList, Widget? _) {
+        return ListView.separated(
+          padding: const EdgeInsets.all(10),
+          //values
+          itemBuilder: (ctx, index) {
+            final _value = newList[index];
+            return Card(
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 50,
+                  child: Text(
+                    parseDate(_value.date),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  backgroundColor: _value.type == CategoryType.income
+                      ? Colors.green
+                      : Colors.red,
+                ),
+                title: Text('Rs.${_value.amount}'),
+                subtitle: Text(_value.category.name),
+                trailing: IconButton(
+                  color: Colors.red,
+                  onPressed: () {},
+                  icon: const Icon(Icons.delete),
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (ctx, index) {
+            return const SizedBox(
+              height: 10,
+            );
+          },
+          itemCount: newList.length,
         );
       },
-      separatorBuilder: (ctx, index) {
-        return const SizedBox(
-          height: 10,
-        );
-      },
-      itemCount: 10,
     );
+  }
+
+  String parseDate(DateTime date) {
+    final _date = DateFormat.MMMd().format(date);
+    final _splitedDate = _date.split(' ');
+    return '${_splitedDate.last}\n${_splitedDate.first}';
+    //return '${date.day}\n${date.month}';
   }
 }
